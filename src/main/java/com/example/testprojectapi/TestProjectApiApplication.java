9a2ceb10050633e5464dc004710cc380f787e962
Clara Kraus <claramariakrausgmail.com>
2022-06-01 16:07:35 +0200

    initialisierung

---
+++ src/main/java/com/example/testprojectapi/TestProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.testprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestProjectApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testprojectapi/TestProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
